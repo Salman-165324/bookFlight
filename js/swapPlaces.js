f60c3fdeb0fdf8bad0c48a5c5f84c2d840d904ab
salman165324 <salmansadi165324@gmail.com>
2024-04-22 04:20:26 +0600

    Edit the swap function with the right id for the fields

--- js/swapPlaces.js
+++ js/swapPlaces.js
@@ -1,6 +1,6 @@
 const swapValues = () => {
-    const departureField = document.getElementById('departure-field');
-    const destinationField = document.getElementById('destination-field');
+    const departureField = document.getElementById('departure-input');
+    const destinationField = document.getElementById('destination-input');
     const temp = departureField.value;
     departureField.value = destinationField.value;
     destinationField.value = temp;
